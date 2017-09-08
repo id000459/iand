994cb5f9190305e358184221b6f87be55d4e605b
id000459 <id000459@gmail.com>
2017-09-07 21:54:22 -0400

    First commit

---
+++ js/newlistingscript.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+  var compilation = function (url, tplId, anchor) {
+       $.getJSON(url, function(data) {
+
+
+                var template = $(tplId).html();
+                var stone = Handlebars.compile(template)(data);
+                $(anchor).append(stone);
+
+
+
+
+       });
+    }
+      //4b.function firing
+      compilation('../newjobs.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
+
+
+
+    var  modalFunction  = function (index) {
+      $.getJSON('../jobs.json', function(data) {
+        var jsonIndexData = data[index];
+        var template = $('#Job-Modal').html();
+        var modalData = Handlebars.compile(template)(jsonIndexData);
+        $(anchor).append(modalData);
+        alert("I work");
+      });
+    }
+
+
+
+
+
+
+
+
+}); // end ready
