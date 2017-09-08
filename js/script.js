c74e0739c4506dd741482f0a134e526265529634
id000459 <id000459@gmail.com>
2017-09-07 22:52:18 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,12 +15,12 @@ $(document).ready(function(){
        });
     }
       //4b.function firing
-      compilation('../jobs.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
+      compilation('../id000459/jobs.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
 
 
 
     var  modalFunction  = function (index) {
-      $.getJSON('../jobs.json', function(data) {
+      $.getJSON('../id000459/jobs.json', function(data) {
         var jsonIndexData = data[index];
         var template = $('#Job-Modal').html();
         var modalData = Handlebars.compile(template)(jsonIndexData);
