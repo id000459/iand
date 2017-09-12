a0b9af3cf920bc52727b638822c97fdafeef67cb
id000459 <id000459@gmail.com>
2017-09-11 20:14:12 -0400

    c

--- js/script.js
+++ js/script.js
@@ -19,16 +19,7 @@ $(document).ready(function(){
 
 
 
-    var  modalFunction  = function (index) {
-      $.getJSON('../id000459/jobs.json', function(data) {
-        var jsonIndexData = data[index];
-        var template = $('#Job-Modal').html();
-        var modalData = Handlebars.compile(template)(jsonIndexData);
-        $(anchor).append(modalData);
-        alert("I work");
-      });
-    }
-
+    
 
 
 
