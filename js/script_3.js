84054c81bf43cb8073ef8daeae2bfa652bdfb6ef
id000459 <id000459@gmail.com>
2017-09-17 17:10:13 -0400

    c

--- js/script_3.js
+++ js/script_3.js
@@ -18,22 +18,3 @@ $(document).ready(function(){
       compilation('../id000459/link3.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
 
 
-/* 
-    var  modalFunction  = function (index) {
-      $.getJSON('../id000459/jobs.json', function(data) {
-        var jsonIndexData = data[index];
-        var template = $('#Job-Modal').html();
-        var modalData = Handlebars.compile(template)(jsonIndexData);
-        $(anchor).append(modalData);
-        alert("I work");
-      });
-    } */
-
-
-
-
-
-
-
-
-}); // end ready
