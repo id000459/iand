70f3c3556cce3ba52fd08e6c56ea92bd9d4ac080
id000459 <id000459@gmail.com>
2017-10-08 16:15:25 -0400

    d

---
+++ milestone1/js/script_4.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+  var compilation = function (url, tplId, anchor) {
+       $.getJSON(url, function(data) {
+
+
+                var template = $(tplId).html();
+                var stone = Handlebars.compile(template)(data);
+                $(anchor).append(stone);
+
+
+
+
+       });
+    }
+      //4b.function firing
+      compilation('../id000459/link4.json', '#listing-template', '#listing-container'); // since url = 'data.json' , we can use both notations.
+
+
+
+
+
+
+
+}); // end ready
