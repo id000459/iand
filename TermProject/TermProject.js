61488a902c652b1f70055ea53cd9cdc8724b5364
id000459 <id000459@gmail.com>
2017-11-30 15:59:28 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -5,7 +5,7 @@ $(document).ready(function ()
 	$("#btnSearch").click(function ()
 	{
 		var url="https://api.themoviedb.org/3/search/movie?api_key=2034377edd6aba446d2cd930085ab35f&query=" + $("#searchTerm").val();
-		searchmovies(url, "moviegridtemplate","movielist");
+		searchmovies(url, "movielisttemplate","movielist");
 	});
 	 
 	$("#btnList").click(function ()
