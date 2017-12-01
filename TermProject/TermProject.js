671dcd3e184e8d8e79a74c46c37fb5a0467ecfb5
id000459 <id000459@gmail.com>
2017-12-01 15:07:34 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -187,7 +187,6 @@ function getpop(movieid)
 
 function getpopmovieDetails(movieid, div)
 {
-	 $("#moviedetails").html("Working ..."+"<img src='http://spiralforums.biz/uploads/monthly_09_2010/post-2-1283575897.gif'>");
 
 	 $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=2034377edd6aba446d2cd930085ab35f" , function (jsonData)
 	 {
