61a7d905d74413e09664e49e63e87440a0c33cf5
id000459 <id000459@gmail.com>
2017-12-01 14:56:43 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -185,6 +185,17 @@ function getpop(movieid)
 	});
 }
 
+function getpopmovieDetails(movieid, div)
+{
+	 $("#moviedetails").html("Working ..."+"<img src='http://spiralforums.biz/uploads/monthly_09_2010/post-2-1283575897.gif'>");
 
+	 $.getJSON("https://api.themoviedb.org/3/movie/" + movieid + "?api_key=2034377edd6aba446d2cd930085ab35f" , function (jsonData)
+	 {
+		var template = $('#pop1').html();
+		var html = Mustache.render(template, jsonData);
+		$(div).html(html);
+		$(div).slideToggle();
+	 });
+}
 
 		
