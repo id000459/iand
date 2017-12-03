d502ac669b33f179a17c6c386e98fd6dbb5e5c09
id000459 <id000459@gmail.com>
2017-12-03 12:36:07 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -270,7 +270,7 @@ function populateddlist()
 		$('#ddlist').html(html);
 	 });
 	 
-	 $(".ddlistitem").click(function(e)
+	 $(".ddlistitem").on('click', function () 
 	{ 
 		id=$(this).attr("genreid");
 		var url="https://api.themoviedb.org/3/search/movie?api_key=2034377edd6aba446d2cd930085ab35f&query=" + $("#searchTerm").val();
