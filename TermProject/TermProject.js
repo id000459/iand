035a518b81f36268b4c12ce9a40e33e68560ae98
id000459 <id000459@gmail.com>
2017-12-03 17:15:22 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -51,7 +51,60 @@ $(document).ready(function ()
 	});					 
   */
   
-	
+	var moveLeft = 0;
+	var moveDown = 0;
+	$('a.popper').hover(function (e) {
+		var target = '#' + ($(this).attr('data-popbox'));
+		var popuptype = $(this).attr('poptype');
+		
+		if (popuptype == 'company') {
+			getcompanymovieDetails(movieid, target);
+		}
+		
+		$(target).show();
+		
+		moveLeft = $(this).outerWidth();
+		moveDown = ($(target).outerHeight() / 2);
+	}, function () {
+		var target = '#' + ($(this).attr('data-popbox'));
+		if (!($("a.popper").hasClass("show"))) {
+			$(target).hide();
+		}
+	});
+
+	$('a.popper').mousemove(function (e) {
+		var target = '#' + ($(this).attr('data-popbox'));
+
+		leftD = e.pageX + parseInt(moveLeft);
+		maxRight = leftD + $(target).outerWidth();
+		windowLeft = $(window).width() - 40;
+		windowRight = 0;
+		maxLeft = e.pageX - (parseInt(moveLeft) + $(target).outerWidth() + 20);
+
+		if (maxRight > windowLeft && maxLeft > windowRight) {
+			leftD = maxLeft;
+		}
+
+		topD = e.pageY - parseInt(moveDown);
+		maxBottom = parseInt(e.pageY + parseInt(moveDown) + 20);
+		windowBottom = parseInt(parseInt($(document).scrollTop()) + parseInt($(window).height()));
+		maxTop = topD;
+		windowTop = parseInt($(document).scrollTop());
+		if (maxBottom > windowBottom) {
+			topD = windowBottom - $(target).outerHeight() - 20;
+		} else if (maxTop < windowTop) {
+			topD = windowTop + 20;
+		}
+
+		$(target).css('top', topD).css('left', leftD);
+	});
+	$('a.popper').click(function (e) {
+		var target = '#' + ($(this).attr('data-popbox'));
+		if (!($(this).hasClass("show"))) {
+			$(target).show();
+		}
+		$(this).toggleClass("show");
+	});
  
  
 });
@@ -182,7 +235,7 @@ function tvpageClick(buttonNumber)
 	}
 }
 
-function getpop(movieid)
+/* function getpop(movieid)
 {
 	
 	var moveLeft = 0;
@@ -239,7 +292,7 @@ function getpop(movieid)
 		}
 		$(this).toggleClass("show");
 	});
-}
+} */
 
 function getcompanymovieDetails(movieid, div)
 {
