c5754eb10fd5cb38950fa3db1852578312c9800c
id000459 <id000459@gmail.com>
2017-12-02 19:08:06 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -141,7 +141,7 @@ function getpop(movieid)
 		var target = '#' + ($(this).attr('data-popbox'));
 		$(target).show();
 		/* getpopmovieDetails(movieid, target); */
-		$( "#poptitle" ).html("{{title}}");
+		/* $( "#poptitle" ).html("{{title}}"); */
 		moveLeft = $(this).outerWidth();
 		moveDown = ($(target).outerHeight() / 2);
 	}, function () {
