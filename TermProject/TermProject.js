f84368d36fc8e7620aa321b355ba14da0811d495
id000459 <id000459@gmail.com>
2017-12-03 20:10:52 -0500

    s

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -58,6 +58,8 @@ $(document).ready(function ()
 
 function searchmovies(servicePoint, templatetype, elementname)
 {
+	openTab(event, 'filtertab');
+	
 	$("#" + elementname).html("Searching ..."+"<img src='http://spiralforums.biz/uploads/monthly_09_2010/post-2-1283575897.gif'>");
 	searchtype = $('.selectlist').val();
 	
