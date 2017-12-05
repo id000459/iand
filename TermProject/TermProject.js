aa32a9865c06f85d0bbcc35db266d6c03f91a0fe
id000459 <id000459@gmail.com>
2017-12-04 22:43:55 -0500

    s

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -146,7 +146,7 @@ function searchmovies(servicePoint, templatetype, elementname)
 		$(".genrelistitemheader").on('click', function () 
 		{ 
 			div=$(this).next(); // get the tv details div
-			getgenreDetails($(this).attr("data-genreid"), div);
+			getmovieDetails($(this).attr("data-genreid"), div);
 		});	
 		$(".boxitem").on('click', function () 
 		{ 
