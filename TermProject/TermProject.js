2519ac6568fefb1398c7d41ac2aa83cc21ac8e43
id000459 <id000459@gmail.com>
2017-12-05 14:17:08 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -52,6 +52,33 @@ $(document).ready(function ()
 				moveLeft = $(target).outerWidth();
 				moveDown = ($(target).outerHeight() / 2);
 			});
+			
+			$('a.popper').mousemove(function (e) {
+				var target = '#' + ($(this).attr('data-popbox'));
+
+				leftD = e.pageX + parseInt(moveLeft);
+				maxRight = leftD + $(target).outerWidth();
+				windowLeft = $(window).width() - 40;
+				windowRight = 0;
+				maxLeft = e.pageX - (parseInt(moveLeft) + $(target).outerWidth() + 20);
+
+				if (maxRight > windowLeft && maxLeft > windowRight) {
+					leftD = maxLeft;
+				}
+
+				topD = e.pageY - parseInt(moveDown);
+				maxBottom = parseInt(e.pageY + parseInt(moveDown) + 20);
+				windowBottom = parseInt(parseInt($(document).scrollTop()) + parseInt($(window).height()));
+				maxTop = topD;
+				windowTop = parseInt($(document).scrollTop());
+				if (maxBottom > windowBottom) {
+					topD = windowBottom - $(target).outerHeight() - 20;
+				} else if (maxTop < windowTop) {
+					topD = windowTop + 20;
+				}
+
+				$(target).css('top', topD).css('left', leftD);
+			});
 		}
 		if (popuptype == 'cast') {
 			getcastDetails($(this).attr('id'), target);
