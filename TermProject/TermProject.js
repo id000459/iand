c78a5214ce897c28ac4a830d071333560af77248
id000459 <id000459@gmail.com>
2017-12-06 00:05:37 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -167,8 +167,8 @@ $(document).ready(function ()
 			searchmovies(url, "movielisttemplate","movielist");
 		} else {
 			if ($( "#myselect" ).val() == "Movies In Theaters Now") {
-				$.getJSON('https://api.themoviedb.org/3/discover/' + searchtype + '?api_key=2034377edd6aba446d2cd930085ab35f&primary_release_date.gte=' + (date.getMonth() - 1 ) + '/' + (date.getDate()) + '/' + (date.getFullYear()) + '&primary_release_date.lte=' + Date() , function(book) {
-				
+				$.getJSON('https://api.themoviedb.org/3/discover/' + searchtype + '?api_key=2034377edd6aba446d2cd930085ab35f&primary_release_date.gte=' + (date.getMonth() - 1 ) + '-' + (date.getDate()) + '-' + (date.getFullYear()) + '&primary_release_date.lte=' + Date() , function(book) {
+				}
 			}
 			
 		}
