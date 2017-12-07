29e943408e968720bff4e3d55533530504b2497b
id000459 <id000459@gmail.com>
2017-12-07 16:38:58 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -280,7 +280,7 @@ $(document).ready(function ()
 	
 	$("#btnListtv").click(function ()
 	{ 
-		var url="https://api.themoviedb.org/3/" + moviesearchtype + "/movie?api_key=2034377edd6aba446d2cd930085ab35f&include_adult=" + includeadultmovies + "&query=" + $("#searchTerm").val();
+		var url="https://api.themoviedb.org/3/" + moviesearchtype + "/" + searchtype + "?api_key=2034377edd6aba446d2cd930085ab35f&include_adult=" + includeadultmovies + "&query=" + $("#searchTerm").val();
 		searchmovies(url, "tvlisttemplate","tvlist");
 		templatetype = "list";
 	});
