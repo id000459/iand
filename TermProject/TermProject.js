6e933f1a9a7e9863a17dc2a2738777c42f12c944
id000459 <id000459@gmail.com>
2017-12-07 19:10:48 -0500

    d

--- TermProject/TermProject.js
+++ TermProject/TermProject.js
@@ -399,7 +399,7 @@ function getTVDetails(movieid, div)
 	 
 }
 
-function getGridDetails(movieid)
+function getGridDetails(movieid, div)
 {
 	 $("#movieDetail").html("Working ..."+"<img src='http://spiralforums.biz/uploads/monthly_09_2010/post-2-1283575897.gif'>");
 	 $.getJSON("https://api.themoviedb.org/3/" + searchtype + "/" + movieid + "?api_key=2034377edd6aba446d2cd930085ab35f" , function (jsonData)
